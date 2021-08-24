37def96e2e39da37b20decdb072f419b3f4fb9b3
Vinicius Azevedo <vinicius.azevedo@fhinck.com>
2021-08-24 16:08:59 -0300

    Subindo para o repositorio

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd4957f7436f983985f5f608d5866b40f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'af2d9d00-7ca1-45ad-83d1-d45f0d458b7c'
+};
